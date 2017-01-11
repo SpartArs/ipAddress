e603dba66bd5c0ec49bc3b47ff7573f20c4a788a
SpartArs <Spartars@yandex.ru>
2017-01-11 21:28:54 +0300

    Start project

---
+++ src/main/java/ipaddress/IPaddress.java
@@ -0,0 +1,11 @@
+package ipaddress;
+
+public class IPaddress {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the first IP: ");
+		System.out.println("Enter the second IP: ");
+
+	}
+
+}
