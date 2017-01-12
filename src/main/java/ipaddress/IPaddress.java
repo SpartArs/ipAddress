45565f6337f0ae7d1f6ebe049de19de12d5376dc
SpartArs <Spartars@yandex.ru>
2017-01-12 16:12:59 +0300

    Second commit(Conversion ip to long)

--- src/main/java/ipaddress/IPaddress.java
+++ src/main/java/ipaddress/IPaddress.java
@@ -1,11 +1,32 @@
 package ipaddress;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class IPaddress {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
 		System.out.println("Enter the first IP: ");
+		String firstIP = reader.readLine();
+		long i = ipToLong(firstIP);
+
 		System.out.println("Enter the second IP: ");
+		String secondIP = reader.readLine();
+		long j = ipToLong(secondIP);
 
 	}
 
+	public static long ipToLong(String ip) {
+
+		String[] ipSplit = ip.split("\\.");
+		return ((Long.parseLong(ipSplit[0]) << 24)) +
+				((Long.parseLong(ipSplit[1]) << 16)) +
+				((Long.parseLong(ipSplit[2]) << 8)) +
+				(Long.parseLong(ipSplit[3]));
+
+	}
 }
